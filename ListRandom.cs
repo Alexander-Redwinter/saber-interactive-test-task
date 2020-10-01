435788922d6f7613e6de4b08f61fad25e24149d7
Seichirino <likeanopenwindow@gmail.com>
2020-10-01 10:40:42 +0300

    Add project files.

---
+++ ListRandom.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SaberInteractiveTestTask
+{
+    class ListNode
+    {
+        public ListNode Previous;
+        public ListNode Next;
+        public ListNode Random; // произвольный элемент внутри списка
+        public string Data;
+    }
+    class ListRandom
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        /// <summary>
+        /// Serialize this <see cref="ListRandom"/> to memory stream.
+        /// </summary>
+        /// <param name="s">Stream to serialize to.</param>
+        public void Serialize(Stream s)
+        {
+            //i tried to get O(n) but i think with Random parameter it is not possible. Currently serializing is O(n^2)
+            var list = new List<ListNode>();
+
+            ListNode currentNode = Head;
+
+            while (currentNode != null)
+            {
+                if (list.Contains(currentNode))
+                    throw new ArgumentException("List has a loop.");
+
+                list.Add(currentNode);
+
+                currentNode = currentNode.Next;
+            }
+
+            StringBuilder dataBuilder = new StringBuilder();
+
+            //using "Data=" and "Random=" as separators
+            foreach (ListNode node in list)
+            {
+                dataBuilder.Append($"Data={node.Data}Random={list.IndexOf(node.Random)}");
+            }
+
+            byte[] bytes = new UTF8Encoding(true).GetBytes(dataBuilder.ToString());
+            s.Write(bytes, 0, bytes.Length);
+        }
+
+        /// <summary>
+        /// Deserialize <see cref="ListRandom"/> from memory stream.
+        /// </summary>
+        /// <param name="s">Stream to desirialize from.</param>
+        public void Deserialize(Stream s)
+        {
+            List<ListNode> list = new List<ListNode>();
+            s.Position = 0;
+            string data;
+
+            using (StreamReader reader = new StreamReader(s, Encoding.UTF8))
+                data = reader.ReadToEnd();
+
+            string[] arr = data.Split(new string[] { "Data=" }, StringSplitOptions.RemoveEmptyEntries);
+
+            Head = new ListNode();
+            ListNode currentNode = Head;
+
+            Count = arr.Length;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                currentNode.Data = arr[i];
+                list.Add(currentNode);
+
+                ListNode next = new ListNode();
+                currentNode.Next = next;
+                next.Previous = currentNode;
+
+                currentNode = next;
+            }
+
+            Tail = currentNode.Previous;
+            Tail.Next = null;
+
+            foreach (ListNode n in list)
+            {
+                var tempArr = n.Data.Split(new string[] { "Random=" }, StringSplitOptions.None);
+                n.Random = list[Convert.ToInt32(tempArr[1])];
+                n.Data = tempArr[0];
+            }
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace SaberInteractiveTestTask
+{
+    class Program
+    {
+        static readonly Random rng = new Random();
+
+        static void Main(string[] args)
+        {
+            int length = 12;
+
+            ListNode head = new ListNode();
+            ListNode tail;
+            ListNode currentNode;
+
+            head.Data = Guid.NewGuid().ToString();
+
+            tail = head;
+
+            for (int i = 0; i < length - 1; i++)
+                tail = AddNode(tail);
+
+            currentNode = head;
+
+            for (int i = 0; i < length; i++)
+            {
+                currentNode.Random = GetRandomNode(head, length);
+                currentNode = currentNode.Next;
+            }
+
+            ListRandom randomList = new ListRandom
+            {
+                Head = head,
+                Tail = tail,
+                Count = length
+            };
+
+            //Will use in-memory stream
+            Stream stream = new MemoryStream();
+            randomList.Serialize(stream);
+
+            ListRandom resultList = new ListRandom();
+
+            resultList.Deserialize(stream);
+
+            if (DeepCompareLists(randomList, resultList, true))
+                Console.WriteLine("Lists are equal.");
+            else
+                Console.WriteLine("Lists are not equal.");
+
+
+            Console.Read();
+
+        }
+
+        /// <summary>
+        /// Deeply compares two <see cref="ListRandom"/>
+        /// </summary>
+        /// <param name="first">First list.</param>
+        /// <param name="second">Second list.</param>
+        /// <param name="print">Print node data to console.</param>
+        /// <returns>True if lists are deeply equal, false otherwise.</returns>
+        static bool DeepCompareLists(ListRandom first, ListRandom second, bool print)
+        {
+            ListNode firstNode = first.Head;
+            ListNode secondNode = second.Head;
+
+            while (firstNode != null)
+            {
+                if(print)
+                    Console.WriteLine($"{firstNode.Data} | {secondNode.Data}");
+
+                if (secondNode == null)
+                    return false;
+
+                if (firstNode.Data != secondNode.Data && firstNode.Random.Data != secondNode.Random.Data)
+                    return false;
+
+                firstNode = firstNode.Next;
+                secondNode = secondNode.Next;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Create new node and set it as next to node from input.
+        /// </summary>
+        /// <param name="node">Parent node.</param>
+        /// <returns>New node.</returns>
+        static ListNode AddNode(ListNode node)
+        {
+            ListNode newNode = new ListNode
+            {
+                Previous = node,
+                Next = null,
+                Data = Guid.NewGuid().ToString()
+            };
+            node.Next = newNode;
+            return newNode;
+        }
+
+        /// <summary>
+        /// Returns a random node from linked list that starts with node from input.
+        /// </summary>
+        /// <param name="head">Head node of linked list.</param>
+        /// <param name="length">Linked list length.</param>
+        /// <returns>Random node.</returns>
+        static ListNode GetRandomNode(ListNode head, int length)
+        {
+            int random = rng.Next(0, length);
+
+            ListNode node = head;
+
+            for (int i = 0; i < random; i++)
+            {
+                if (node == null)
+                    throw new ArgumentException("Linked list length is incorrect.");
+
+                node = node.Next;
+            }
+
+            return node;
+        }
+    }
+}
